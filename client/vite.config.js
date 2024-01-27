0da476fee5d8a22d446875be85725e2d40eea7cc
Rogelio <roger.cruz808@gmail.com>
2024-01-26 16:28:18 -0800

    server running

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default {
   plugins: [react()],
   server: {
     proxy: {
-      "/api": `http://localhost:${process.env.PORT}`,
+      "/api": `http://localhost:${process.env.EXPRESSPORT}`,
     },
   },
   cacheDir: "../node_modules/.vite",
